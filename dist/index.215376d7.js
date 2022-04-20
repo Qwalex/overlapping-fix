d94a64af6df787e04663fc39e7f31a271e5b9f6e
Aleksey Kovakin <qwazik21@gmail.com>
2022-04-20 09:42:45 +0300

    to integrate

--- dist/index.215376d7.js
+++ dist/index.215376d7.js
@@ -514,143 +514,182 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"iGpzc":[function(require,module,exports) {
-const circle1 = document.getElementById('c1');
-const circle2 = document.getElementById('c2');
-const circle3 = document.getElementById('c3');
-const img = document.querySelector('.img');
-const imgClientRect = img.getBoundingClientRect();
-let circlePlaceholders = [];
-let nearPositions = [];
-const line = document.querySelector('.line');
-img.addEventListener('mousemove', (e)=>{
-    console.log(checkPosition(e)) //false|true|position
-    ;
-});
-img.addEventListener('click', (e)=>{
-    console.log(checkPosition(e)) //false|true|position
-    ;
-});
-const checkPosition = (e)=>{
-    let [offsetX, offsetY] = getOffsetByImage(e);
-    let canAdd = true;
-    if (circlePlaceholders.length > 0) {
-        circlePlaceholders.forEach((circle)=>circle.remove()
-        );
-        circlePlaceholders = [];
-    }
-    nearPositions = [];
-    const eventData = {
-        centerX: offsetX,
-        centerY: offsetY,
-        rad: 50
-    };
-    getAllCircles().forEach((circle)=>{
-        circle.el.classList.remove('red');
-        const distance = getDistance(circle, eventData);
-        if (distance < 0) {
-            const angle = getAngle(circle, eventData);
-            circlePlaceholders.push(createCircle(getShiftCoords([
-                offsetX,
-                offsetY
-            ], angle, Math.abs(distance))));
-            nearPositions.push(getShiftCoords([
-                offsetX,
-                offsetY
-            ], angle, Math.abs(distance)));
-            getShiftCoords([
-                offsetX,
-                offsetY
-            ], angle, Math.abs(distance));
-            circle.el.classList.add('red');
-            line.style.transform = `rotate(${angle}deg)`;
-            line.style.width = `${Math.abs(distance)}px`;
-            canAdd = false;
+window.g = (()=>{
+    const circle11 = document.getElementById('c1');
+    const circle21 = document.getElementById('c2');
+    const circle3 = document.getElementById('c3');
+    const img = document.querySelector('.img');
+    const imgClientRect = img.getBoundingClientRect();
+    const rad1 = 51;
+    let circlePlaceholders = [];
+    let nearPositions = [];
+    const line = document.querySelector('.line');
+    img.addEventListener('mousemove', (e)=>{
+        const availablePosition = checkPosition(e);
+        if (!availablePosition) document.body.classList.add('can-not-add');
+        else document.body.classList.remove('can-not-add');
+    });
+    img.addEventListener('click', (e)=>{
+        console.log('click');
+        const availablePosition = checkPosition(e);
+        console.log(availablePosition);
+        if (!availablePosition) return false;
+        console.log(availablePosition);
+        createCircle(availablePosition[0]);
+    });
+    const checkPosition = (e)=>{
+        let [offsetX, offsetY] = getOffsetByImage(e);
+        let canAdd = true;
+        if (circlePlaceholders.length > 0) {
+            circlePlaceholders.forEach((circle)=>circle.remove()
+            );
+            circlePlaceholders = [];
         }
-    });
-    if (!canAdd) return getAvailablePosition();
-    return true;
-};
-const getCircle = (id)=>{
-    const el = document.getElementById(id);
-    const { left , right , top , bottom , width , height ,  } = el.getBoundingClientRect();
-    const rad = width / 2;
-    return {
-        el,
-        top: top - imgClientRect.top - 1,
-        right,
-        bottom,
-        left: left - imgClientRect.left - 1,
-        width,
-        height,
-        rad,
-        centerX: left - imgClientRect.left - 1 + rad,
-        centerY: top - imgClientRect.top - 1 + rad
-    };
-};
-const getDistance = (circle11, circle21)=>{
-    const { centerX: x1 , centerY: y1 , rad ,  } = circle11;
-    const { centerX: x2 , centerY: y2 ,  } = circle21;
-    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)) - rad * 2;
-};
-const getAngle = (circle12, circle22)=>{
-    const { centerX: x1 , centerY: y1 , rad ,  } = circle12;
-    const { centerX: x2 , centerY: y2 ,  } = circle22;
-    return radiansToDegrees(Math.atan2(y2 - y1, x2 - x1));
-};
-const getAngleCss = (circle13, circle23)=>{
-    return getAngle(circle13, circle23) - 180;
-};
-const getAllCircles = ()=>{
-    return [
-        'c1',
-        'c2',
-        'c3'
-    ].map((id)=>getCircle(id)
-    );
-};
-const createCircle = ([offsetX, offsetY])=>{
-    const el = document.createElement('div');
-    el.classList.add('circle');
-    el.style.left = `${offsetX}px`;
-    el.style.top = `${offsetY}px`;
-    img.appendChild(el);
-    return el;
-};
-const getOffsetByImage = (e)=>{
-    return [
-        e.clientX - imgClientRect.left,
-        e.clientY - imgClientRect.top
-    ];
-};
-const radiansToDegrees = (radians)=>{
-    return radians * (180 / Math.PI);
-};
-const degreesToRadians = (degrees)=>{
-    return degrees * (Math.PI / 180);
-};
-const getShiftCoords = ([x, y], angle, distance)=>{
-    return [
-        x + distance * Math.cos(degreesToRadians(angle)),
-        y + distance * Math.sin(degreesToRadians(angle))
-    ];
-};
-const getAvailablePosition = ()=>{
-    if (nearPositions.length === 0) return true;
-    return nearPositions.filter(([centerX, centerY])=>{
-        const hasIntersections = [];
+        nearPositions = [];
+        const eventData = {
+            centerX: offsetX,
+            centerY: offsetY,
+            rad: rad1
+        };
         getAllCircles().forEach((circle)=>{
-            const distance = getDistance(circle, {
-                centerX,
-                centerY,
-                rad: 50
-            });
-            console.log(distance);
-            if (distance < 0) hasIntersections.push(true);
+            circle.el.classList.remove('red');
+            const distance = getDistance(circle, eventData);
+            if (distance < 0) {
+                const angle = getAngle(circle, eventData);
+                circlePlaceholders.push(createCircle(getShiftCoords([
+                    offsetX,
+                    offsetY
+                ], angle, Math.abs(distance)), true));
+                nearPositions.push(getShiftCoords([
+                    offsetX,
+                    offsetY
+                ], angle, Math.abs(distance)));
+                getShiftCoords([
+                    offsetX,
+                    offsetY
+                ], angle, Math.abs(distance));
+                circle.el.classList.add('red');
+                line.style.transform = `rotate(${angle}deg)`;
+                line.style.width = `${Math.abs(distance)}px`;
+                canAdd = false;
+            }
         });
-        console.log(hasIntersections);
-        return hasIntersections.length === 0;
-    });
-};
+        if (!canAdd) return getAvailablePosition();
+        return [
+            [
+                offsetX,
+                offsetY
+            ]
+        ];
+    };
+    const getCircle = (el)=>{
+        const { left , right , top , bottom , width , height ,  } = el.getBoundingClientRect();
+        const rad = width / 2;
+        return {
+            el,
+            top: top - imgClientRect.top - 1,
+            right,
+            bottom,
+            left: left - imgClientRect.left - 1,
+            width,
+            height,
+            rad,
+            centerX: left - imgClientRect.left - 1 + rad,
+            centerY: top - imgClientRect.top - 1 + rad
+        };
+    };
+    const getDistance = (circle1, circle2)=>{
+        const { centerX: x1 , centerY: y1 , rad ,  } = circle1;
+        const { centerX: x2 , centerY: y2 ,  } = circle2;
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)) - rad * 2;
+    };
+    const getAngle = (circle1, circle2)=>{
+        const { centerX: x1 , centerY: y1 , rad ,  } = circle1;
+        const { centerX: x2 , centerY: y2 ,  } = circle2;
+        return radiansToDegrees(Math.atan2(y2 - y1, x2 - x1));
+    };
+    const getAngleCss = (circle1, circle2)=>{
+        return getAngle(circle1, circle2) - 180;
+    };
+    const getAllCircles = ()=>{
+        return [
+            ...document.querySelectorAll('.js-circle')
+        ].map((el)=>getCircle(el)
+        );
+    };
+    const createCircle = ([offsetX, offsetY], placeholder = false)=>{
+        console.log('createCircle');
+        const el = document.createElement('div');
+        el.classList.add('circle');
+        if (!placeholder) el.classList.add('js-circle');
+        el.style.left = `${offsetX}px`;
+        el.style.top = `${offsetY}px`;
+        img.appendChild(el);
+        return el;
+    };
+    const getOffsetByImage = (e)=>{
+        return [
+            e.clientX - imgClientRect.left,
+            e.clientY - imgClientRect.top
+        ];
+    };
+    const radiansToDegrees = (radians)=>{
+        return radians * (180 / Math.PI);
+    };
+    const degreesToRadians = (degrees)=>{
+        return degrees * (Math.PI / 180);
+    };
+    const getShiftCoords = ([x, y], angle, distance)=>{
+        const additionalOffset = 1;
+        return [
+            x + (distance + additionalOffset) * Math.cos(degreesToRadians(angle)),
+            y + (distance + additionalOffset) * Math.sin(degreesToRadians(angle))
+        ];
+    };
+    const getAvailablePosition = ()=>{
+        // console.clear();
+        // console.log('++++++++++++++++');
+        // console.group('getAvailablePosition');
+        if (nearPositions.length === 0) return true;
+        const result = nearPositions.filter(([centerX, centerY])=>{
+            // console.log('-----------------');
+            // console.log(`check positions ${centerX} ${centerY}`);
+            const hasIntersections = [];
+            getAllCircles().forEach((circle)=>{
+                // console.log('=================');
+                // console.log({circle});
+                const distance = getDistance(circle, {
+                    centerX,
+                    centerY,
+                    rad: rad1
+                });
+                // console.log(`distance: ${distance}`);
+                // console.log(distance);
+                if (distance < 0) // console.log(`distance incorrect`);
+                // console.log(`intersections push true`);
+                hasIntersections.push(true);
+            // console.log('=================');
+            });
+            // console.log(`hasIntersections: ${hasIntersections}`);
+            // console.log(`return ${hasIntersections.length === 0}`)
+            // console.log('-----------------');
+            return hasIntersections.length === 0;
+        });
+        if (result.length === 0) return console.log('empty result !!!!');
+        return result;
+    };
+    setRandomCircle = ()=>{
+        createCircle([
+            Math.random() * 500,
+            Math.random() * 500
+        ]);
+    };
+    return {
+        getDistance,
+        getCircle,
+        setRandomCircle
+    };
+})();
 
 },{}]},["3VBnf","iGpzc"], "iGpzc", "parcelRequire94c2")
 
--- src/js/app.ts
+++ src/js/app.ts
@@ -1,180 +1,231 @@
-const circle1 = document.getElementById('c1');
-const circle2 = document.getElementById('c2');
-const circle3 = document.getElementById('c3');
-const img: HTMLElement = document.querySelector('.img');
-const imgClientRect = img.getBoundingClientRect();
-
-let circlePlaceholders = [];
-let nearPositions = [];
-const line: HTMLElement = document.querySelector('.line');
-
-img.addEventListener('mousemove', (e: MouseEvent) => {
-    console.log(checkPosition(e)) //false|true|position
-});
-
-img.addEventListener('click', (e: MouseEvent) => {
-    console.log(checkPosition(e)) //false|true|position
-});
-
-const checkPosition = (e: MouseEvent) => {
-    let [offsetX, offsetY] = getOffsetByImage(e);
-    let canAdd = true;
-
-    if (circlePlaceholders.length > 0) {
-        circlePlaceholders.forEach(circle => circle.remove());
-        circlePlaceholders = [];
-    }
-    nearPositions = [];
-
-    const eventData = {
-        centerX: offsetX,
-        centerY: offsetY,
-        rad: 50
-    };
-
-    getAllCircles().forEach((circle) => {
-        circle.el.classList.remove('red');
-
-        const distance = getDistance(circle, eventData);
-
-        if (distance < 0) {
-            const angle = getAngle(circle, eventData);
-            circlePlaceholders.push(createCircle(getShiftCoords([offsetX, offsetY], angle, Math.abs(distance))));
-            nearPositions.push(getShiftCoords([offsetX, offsetY], angle, Math.abs(distance)));
-            getShiftCoords([offsetX, offsetY], angle, Math.abs(distance));
-            circle.el.classList.add('red');
-            line.style.transform = `rotate(${angle}deg)`;
-            line.style.width = `${Math.abs(distance)}px`;
-            canAdd = false;
+window.g = (() => {
+    const circle1 = document.getElementById('c1');
+    const circle2 = document.getElementById('c2');
+    const circle3 = document.getElementById('c3');
+    const img: HTMLElement = document.querySelector('.img');
+    const imgClientRect = img.getBoundingClientRect();
+    const rad = 51;
+    
+    let circlePlaceholders = [];
+    let nearPositions = [];
+    const line: HTMLElement = document.querySelector('.line');
+    
+    img.addEventListener('mousemove', (e: MouseEvent) => {
+        const availablePosition = checkPosition(e)
+        if (!availablePosition) {
+            document.body.classList.add('can-not-add');
+        } else {
+            document.body.classList.remove('can-not-add');
         }
     });
+    
+    img.addEventListener('click', (e: MouseEvent) => {
+        console.log('click')
+        const availablePosition = checkPosition(e)
+        console.log(availablePosition);
+        
+        if (!availablePosition) {
+            return false;
+        }
 
-    if (!canAdd) {
-        return getAvailablePosition();
-    }
-    return true;
-}
+        console.log(availablePosition);
 
-interface circle {
-    left: number,
-    right: number,
-    top: number,
-    bottom: number,
-    width: number,
-    height: number,
-    centerX: number,
-    centerY: number,
-    rad: number
-}
-
-const getCircle = (id: string) => {
-    const el = document.getElementById(id);
-    const {
-        left,
-        right,
-        top,
-        bottom,
-        width,
-        height,
-    } = el.getBoundingClientRect();
-    const rad = width / 2;
-
-    return {
-        el,
-        top: top - imgClientRect.top - 1,
-        right,
-        bottom,
-        left: left - imgClientRect.left - 1,
-        width,
-        height,
-        rad,
-        centerX: (left - imgClientRect.left - 1) + rad,
-        centerY: (top - imgClientRect.top - 1) + rad
-    };
-}
-
-const getDistance = (circle1, circle2) => {
-    const {
-        centerX: x1,
-        centerY: y1,
-        rad,
-    } = circle1;
-
-    const {
-        centerX: x2,
-        centerY: y2,
-    } = circle2;
-
-    return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)) - (rad * 2)
-}
-
-const getAngle = (circle1, circle2) => {
-    const {
-        centerX: x1,
-        centerY: y1,
-        rad,
-    } = circle1;
-
-    const {
-        centerX: x2,
-        centerY: y2,
-    } = circle2;
-
-    return radiansToDegrees(Math.atan2(y2 - y1, x2 - x1));
-}
-
-const getAngleCss = (circle1, circle2) => {
-    return getAngle(circle1, circle2) - 180;
-}
-
-const getAllCircles = () => {
-    return ['c1', 'c2', 'c3'].map((id) => getCircle(id));
-}
-
-const createCircle = ([offsetX, offsetY]):HTMLElement => {
-    const el = document.createElement('div');
-    el.classList.add('circle');
-    el.style.left = `${offsetX}px`;
-    el.style.top = `${offsetY}px`;
-    img.appendChild(el);
-    return el;
-}
-
-const getOffsetByImage = (e: MouseEvent) => {
-    return [e.clientX - imgClientRect.left, e.clientY - imgClientRect.top];
-}
-
-const radiansToDegrees = (radians) => {
-    return radians * (180/Math.PI);
-}
-
-const degreesToRadians = (degrees) => {
-    return degrees * (Math.PI / 180);
-}
-
-const getShiftCoords = ([x, y], angle, distance) => {
-    return [x + distance * Math.cos(degreesToRadians(angle)), y + distance * Math.sin(degreesToRadians(angle))];
-}
-
-const getAvailablePosition = () => {
-    if (nearPositions.length === 0) {
-        return true;
-    }
-
-    return nearPositions.filter(([centerX, centerY]) => {
-        const hasIntersections = [];
-
+        createCircle(availablePosition[0]);
+    });
+    
+    const checkPosition = (e: MouseEvent) => {
+        let [offsetX, offsetY] = getOffsetByImage(e);
+        let canAdd = true;
+    
+        if (circlePlaceholders.length > 0) {
+            circlePlaceholders.forEach(circle => circle.remove());
+            circlePlaceholders = [];
+        }
+        nearPositions = [];
+    
+        const eventData = {
+            centerX: offsetX,
+            centerY: offsetY,
+            rad,
+        };
+    
         getAllCircles().forEach((circle) => {
-            const distance = getDistance(circle, {centerX, centerY, rad: 50});
-            console.log(distance);
-            
+            circle.el.classList.remove('red');
+    
+            const distance = getDistance(circle, eventData);
+    
             if (distance < 0) {
-                hasIntersections.push(true);
+                const angle = getAngle(circle, eventData);
+                circlePlaceholders.push(createCircle(getShiftCoords([offsetX, offsetY], angle, Math.abs(distance)), true));
+                nearPositions.push(getShiftCoords([offsetX, offsetY], angle, Math.abs(distance)));
+                getShiftCoords([offsetX, offsetY], angle, Math.abs(distance));
+                circle.el.classList.add('red');
+                line.style.transform = `rotate(${angle}deg)`;
+                line.style.width = `${Math.abs(distance)}px`;
+                canAdd = false;
             }
         });
+    
+        if (!canAdd) {
+            return getAvailablePosition();
+        }
 
-        console.log(hasIntersections);
+        return [[offsetX, offsetY]];
+    }
+    
+    interface circle {
+        left: number,
+        right: number,
+        top: number,
+        bottom: number,
+        width: number,
+        height: number,
+        centerX: number,
+        centerY: number,
+        rad: number
+    }
+    
+    const getCircle = (el) => {
+        const {
+            left,
+            right,
+            top,
+            bottom,
+            width,
+            height,
+        } = el.getBoundingClientRect();
+        const rad = width / 2;
+    
+        return {
+            el,
+            top: top - imgClientRect.top - 1,
+            right,
+            bottom,
+            left: left - imgClientRect.left - 1,
+            width,
+            height,
+            rad,
+            centerX: (left - imgClientRect.left - 1) + rad,
+            centerY: (top - imgClientRect.top - 1) + rad
+        };
+    }
+    
+    const getDistance = (circle1, circle2) => {
+        const {
+            centerX: x1,
+            centerY: y1,
+            rad,
+        } = circle1;
+    
+        const {
+            centerX: x2,
+            centerY: y2,
+        } = circle2;
+    
+        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)) - (rad * 2)
+    }
+    
+    const getAngle = (circle1, circle2) => {
+        const {
+            centerX: x1,
+            centerY: y1,
+            rad,
+        } = circle1;
+    
+        const {
+            centerX: x2,
+            centerY: y2,
+        } = circle2;
+    
+        return radiansToDegrees(Math.atan2(y2 - y1, x2 - x1));
+    }
+    
+    const getAngleCss = (circle1, circle2) => {
+        return getAngle(circle1, circle2) - 180;
+    }
+    
+    const getAllCircles = () => {
+        return [...document.querySelectorAll('.js-circle')].map(el => getCircle(el));
+    }
+    
+    const createCircle = ([offsetX, offsetY], placeholder = false):HTMLElement => {
+        console.log('createCircle');
+        const el = document.createElement('div');
+        el.classList.add('circle');
+        if (!placeholder) {
+            el.classList.add('js-circle');
+        }
+        el.style.left = `${offsetX}px`;
+        el.style.top = `${offsetY}px`;
+        img.appendChild(el);
+        return el;
+    }
+    
+    const getOffsetByImage = (e: MouseEvent) => {
+        return [e.clientX - imgClientRect.left, e.clientY - imgClientRect.top];
+    }
+    
+    const radiansToDegrees = (radians) => {
+        return radians * (180/Math.PI);
+    }
+    
+    const degreesToRadians = (degrees) => {
+        return degrees * (Math.PI / 180);
+    }
+    
+    const getShiftCoords = ([x, y], angle, distance):[number, number] => {
+        const additionalOffset = 1;
+        return [x + (distance + additionalOffset) * Math.cos(degreesToRadians(angle)), y + (distance + additionalOffset) * Math.sin(degreesToRadians(angle))];
+    }
+    
+    const getAvailablePosition = () => {
+        // console.clear();
+        // console.log('++++++++++++++++');
+        // console.group('getAvailablePosition');
+        if (nearPositions.length === 0) {
+            return true;
+        }
+    
+        const result = nearPositions.filter(([centerX, centerY]) => {
+            // console.log('-----------------');
+            // console.log(`check positions ${centerX} ${centerY}`);
+            const hasIntersections = [];
+    
+            getAllCircles().forEach((circle) => {
+                // console.log('=================');
+                // console.log({circle});
 
-        return hasIntersections.length === 0;
-    });
-}
+                const distance = getDistance(circle, {centerX, centerY, rad});
+                // console.log(`distance: ${distance}`);
+                // console.log(distance);
+                
+                if (distance < 0) {
+                    // console.log(`distance incorrect`);
+                    // console.log(`intersections push true`);
+                    hasIntersections.push(true);
+                }
+                // console.log('=================');
+            });
+    
+            // console.log(`hasIntersections: ${hasIntersections}`);
+            // console.log(`return ${hasIntersections.length === 0}`)
+            // console.log('-----------------');
+            return hasIntersections.length === 0;
+        });
+
+        if (result.length === 0) {
+            return console.log('empty result !!!!');
+        }
+        return result;
+    }
+
+    setRandomCircle = () => {
+        createCircle([Math.random() * 500, Math.random() * 500]);
+    }
+
+    return {
+        getDistance,
+        getCircle,
+        setRandomCircle,
+    }
+})();
